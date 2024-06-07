890d4b925d35b2254087a88b466fcd3bfc2e2e39
battistaennio <ennio.battista99@gmail.com>
2024-06-07 14:19:34 +0200

    Creazione array lista e evento on click

--- Mail/main.js
+++ Mail/main.js
@@ -3,3 +3,14 @@
 //selettore input e button
 const inputMail = document.querySelector("input");
 const verify = document.querySelector("button");
+
+//creazione lista
+const whiteList = ["gino@gmail.com", "pino@gmail.com", "pippo@gmail.com", "pluto@gmail.com", "paperino@gmail.com"]
+
+//creazione evento su click
+verify.addEventListener("click",
+    function () {
+        let userMail = inputMail.value
+        console.log("l'input mail è: ", userMail);
+    }
+)
