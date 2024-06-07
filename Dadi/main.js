60e1b3fe2ba43aac95d63829d33e44f6808efb8e
battistaennio <ennio.battista99@gmail.com>
2024-06-07 18:07:28 +0200

    Creato generatore numero da 1 a 6 per pc e user

--- Dadi/main.js
+++ Dadi/main.js
@@ -1,1 +1,11 @@
 //Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
+//generatore numero casuale pc
+let pcPlay = Math.floor(Math.random() * 6) + 1;
+console.log("Giocata PC: ", pcPlay);
+
+//generatore numero casuale user
+let UserPlay = Math.floor(Math.random() * 6) + 1;
+console.log("Giocata User: ", UserPlay);
+
