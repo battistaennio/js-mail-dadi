262d326964674c57910baa2ed428afebbb212674
battistaennio <ennio.battista99@gmail.com>
2024-06-07 14:16:52 +0200

    Creato form in HTML e selettore input/button in JS

--- Mail/main.js
+++ Mail/main.js
@@ -1,1 +1,5 @@
 //Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+//selettore input e button
+const inputMail = document.querySelector("input");
+const verify = document.querySelector("button");
