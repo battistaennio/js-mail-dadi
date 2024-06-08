bd400fe612379bdcac3a9c462aa58a4e018d2d8a
battistaennio <ennio.battista99@gmail.com>
2024-06-08 13:02:22 +0200

    ESERCIZIO DADI TERMINATO

--- Dadi/main.js
+++ Dadi/main.js
@@ -1,19 +1,36 @@
 //Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
+//selettore button
+const playButton = document.querySelector("button");
 
-//generatore numero casuale pc
-let pcPlay = Math.floor(Math.random() * 6) + 1;
-console.log("Giocata PC: ", pcPlay);
+//selettore output vincita
+let result = document.getElementById("result-output");
 
-//generatore numero casuale user
-let UserPlay = Math.floor(Math.random() * 6) + 1;
-console.log("Giocata User: ", UserPlay);
+//selettore simbolo sfida
+let symbol = document.getElementById("symbol")
 
-//regole del gioco
-if (pcPlay > UserPlay) {
-    console.log("vince pc");
-} else if (pcPlay < UserPlay) {
-    console.log("vince user");
-} else {
-    console.log("parità");
-}
+//il gioco parte al click del button
+playButton.addEventListener("click",
+    function () {
+
+        //generatore numero casuale pc
+        let pcPlay = Math.floor(Math.random() * 6) + 1;
+        document.getElementById("pc-output").innerText = pcPlay;
+    
+        //generatore numero casuale user
+        let userPlay = Math.floor(Math.random() * 6) + 1;
+        document.getElementById("user-output").innerText = userPlay;
+    
+        //regole del gioco
+        if (pcPlay > userPlay) {
+            result.innerText = "Vince il Computer, Ritenta";
+            symbol.innerText = "<"
+        } else if (pcPlay < userPlay) {
+            result.innerText = "Hai Vinto!";
+            symbol.innerText = ">"
+        } else {
+            result.innerText = "Parità";
+            symbol.innerText = "="
+        }
+    }
+)
